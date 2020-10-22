6ef3a7958c36f2a1f0ab68b7812ee11460596e79
Frank T. Bergmann <fbergman@caltech.edu>
2020-10-22 14:37:39 +0200

    - use ImportError

--- basico/compartment_array_tools.py
+++ basico/compartment_array_tools.py
@@ -4,9 +4,7 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 try:
     from PIL import Image
-except NameError:
-    pass
-except ModuleNotFoundError:
+except ImportError:
     pass
 
 

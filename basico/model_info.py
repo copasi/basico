7c242df9b0cf0af4e194d533f86d56511f261e3e
Frank T. Bergmann <fbergman@caltech.edu>
2021-05-11 11:40:24 +0200

    - open in write mode

--- basico/model_info.py
+++ basico/model_info.py
@@ -2413,7 +2413,7 @@ def update_miriam_resources():
     except ImportError:
         from . import biomodels
 
-    with tempfile.NamedTemporaryFile(encoding='utf-8') as temp_file:
+    with tempfile.NamedTemporaryFile(mode='w', encoding='utf-8') as temp_file:
         temp_file.write(biomodels.download_from('http://copasi.org/static/miriam.xml').encode('utf-8'))
         name = temp_file.name
         temp_file.close()
